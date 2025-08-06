b7af478b1cca871e66df8cdff5e031e2dddedc1e
FathimathuShifana <shifana@kiebot.com>
2025-08-06 17:20:21 +0530

    home

--- home.js
+++ home.js
@@ -6,6 +6,8 @@ function home() {
         <h1>Home</h1>
         <p>This is the home page of the e-commerce website.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. So what will we do now? what is your family saying?
 I dont know what to do now. if you come here do you think they will allow me to go? if they didnt what will we do?
+
+I dont know.you say Icant do do anything from here beacause there is always somebody to watch me. if they cauth me once more then I dont know what will happen. I will go know the will come back only in the morning.
 </p>
     </div>
   )
