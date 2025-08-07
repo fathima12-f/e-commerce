68d07e67a856a48a746f9696c290ebb9f07bd779
FathimathuShifana <shifana@kiebot.com>
2025-08-07 11:04:45 +0530

    home-en

--- en.js
+++ en.js
@@ -3,12 +3,7 @@ import React from 'react'
 function en() {
   return (
     <div>
-   <p>inte abba pallil pokule appo kanana pattule . jamathin pokule .</p>
-
-   <p>Yes</p>
-   <p>Daa  ni inn vann kynna pinna ntha aava ank areela ank tension aavn chelppo pinna lap oonu theroola pinna contact cheyyane patoola </p>
-
-        <p>Appo ennh cheyyande . berande . nalle seen aaakua .lap illenkil pinnh ank oru contact illenkil onnu nadakkulallo</p>
+   <p>Daa ank thonnunnilla onnuu nadakoon ank areea nthaaava nnu </p>
 
     </div>
   )
--- home.js
+++ home.js
@@ -4,15 +4,8 @@ function home() {
   return (
     <div>
         <h1>Home</h1>
-        <p>This is the home page of the e-commerce website.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. So what will we do now? what is your family saying?
-I dont know what to do now. if you come here do you think they will allow me to go? if they didnt what will we do?
-
-I dont know.you say Icant do do anything from here beacause there is always somebody to watch me. if they cauth me once more then I dont know what will happen. I will go know the will come back only in the morning.
-
-if they didnt allow me then what will we do? are you coming alone? if coming alone then definitely they wont let me come with you.
-</p>
-
-<p>will say no 100% sure. Okay you come . I dont know what will happen here.im getting tensed what to do? </p>
+        <p>This is the home page of the e-commerce website.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  </p>
     </div>
   )
 }
