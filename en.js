96034571a7a45bddb55aa890b5f3f01653ca353f
FathimathuShifana <shifana@kiebot.com>
2025-08-07 11:20:14 +0530

    en

--- en.js
+++ en.js
@@ -3,10 +3,7 @@ import React from 'react'
 function en() {
   return (
     <div>
-   <p>Daa ank thonnunnilla onnuu nadakoon ank areea nthaaava nnu </p>
-
-   <p>ennh bernnilla vannit seeen aayit ink laaap thannille pinnh onnu paraya patoola</p>
-   <p>vera ennh cheyyande</p>
+   <p>Daa onnu nadakoola ni happy ayyt irikkk aakane</p>
 
 
 
