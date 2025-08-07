c7874f1aebfc078e46a866f2d7029d364eda5847
FathimathuShifana <shifana@kiebot.com>
2025-08-07 11:34:07 +0530

    en

--- en.js
+++ en.js
@@ -3,7 +3,7 @@ import React from 'react'
 function en() {
   return (
     <div>
-   <p>ingane poya na karanam inte lyfum illand aavum </p>
+   <p>QAAmk pattnnilla na ntha cheyyende ellaru annod nthello paraynn ith orikkalu nadkoolan ath mathro ella inna ntyalau ketticheroolaan I ank eni onnu cheyya patoola nthelu nadakkane onnikil inte family prayanam nnalu nadakkuo ank areela  </p>
 
 
 
