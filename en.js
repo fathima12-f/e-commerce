e2ebb6b9c7c64d2ae763e1b3de5b68e89be19855
FathimathuShifana <shifana@kiebot.com>
2025-08-07 10:45:38 +0530

    en

--- en.js
+++ en.js
@@ -4,6 +4,9 @@ function en() {
   return (
     <div>
    <p>inte abba pallil pokule appo kanana pattule . jamathin pokule .</p>
+
+   <p>Yes</p>
+   <p>Daa  ni inn vann kynna pinna ntha aava ank areela ank tension aavn chelppo pinna lap oonu theroola pinna contact cheyyane patoola </p>
     </div>
   )
 }
