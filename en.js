228d0b9229d9236662a2115d6c8bd55d33975c66
FathimathuShifana <shifana@kiebot.com>
2025-08-08 16:13:39 +0530

    h

--- en.js
+++ en.js
@@ -3,21 +3,7 @@ import React from 'react'
 function en() {
   return (
     <div>
-   <p>daa ank pattnnilla na ntha cheyyende ellaru annod nthello paraynn ith orikkalu nadkoolan ath mathro ella inna ntyalau ketticheroolaan I ank eni onnu cheyya patoola nthelu nadakkane onnikil inte family prayanam nnalu nadakkuo ank areela  </p>
-<p>Daa ink areele na enganeya msg aakennen na ntha cheyyende ank onnu manassilavnnilla ni pinna nthekilu cheyya pattue cheyy onnu </p>
-
-
-<p>ok . ni pareene pole . ank onnu paraya kittinnilla</p>
-
-<p> means??</p>
-<p>na nthaa cheyyende ni crct para</p>
-<p>Daa na oru kaaryo parayatte ni korch kynn oru job ello kitteet vann choikkuo athvare nammk contact onnu illand nikka ok aano?? Ni nthekilu para</p>
-
-
-<p> Haa ok</p>
-
-
-<p>Okay. nthekilu inde ithil msg aaka</p>
+   
     </div>
   )
 }
