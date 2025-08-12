2228f3e3d2266e8203a0d6e03ce01f980a36e805
FathimathuShifana <shifana@kiebot.com>
2025-08-12 16:47:46 +0530

    home

--- home.js
+++ home.js
@@ -4,8 +4,7 @@ function home() {
   return (
     <div>
         <h1>Home</h1>
-        <p>This is the home page of the e-commerce website.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
-          somtimes i cnat message after this
+        <p>This is the home page of the e-commerce website.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Tommorow i will not be there and today they came to see mee what should i do now if they say yes then i cannot do anything
         </p>
     </div>
   )
